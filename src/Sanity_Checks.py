df4dc0594f0c7e6431ebed72f4143b90f0266649
Kabir1607 <kabirvohra2005@gmail.com>
2025-12-04 13:32:09 +0530

    Update Sanity_Checks.py

---
+++ src/Sanity_Checks.py
@@ -0,0 +1,6 @@
+# Looking and exploring the dataset to see if all is in order: 
+
+# Counting Number of Artworks per Style in ArtEmis (the csv file):
+# Ignore repetitions, just count the number of unique artworks per style: 
+
+# Counting Number of Artworks per Style in wikiart (the folders): 
